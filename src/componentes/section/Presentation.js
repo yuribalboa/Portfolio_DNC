e7bd00bc384ef2ea743a2dd4e2b20379c47bd8af
YuriTropeia <yurineudinetropeia@gmail.com>
2024-05-13 19:36:49 -0300

    v2

--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -1,12 +1,43 @@
 import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
-import ButtonB from '../elements/ButtonB'
+import {useEffect, useState} from 'react'
 
 function Presentation(){
+    const [text, setText] = useState('');
+    const toRotate = ['Yuri Tropeia!', 'Desenvolvedor Front End!', 'Product Manager!'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 200;
+    const [delta, setDelta] = useState(100);
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            toType()
+        }, delta)
+        return() => {clearInterval(ticker)}
+    }, [text])
+
+    const toType = () =>{
+        let i = loop % toRotate.length
+        let fullText = toRotate[i]
+        let updatedText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+
+        setText(updatedText);
+
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if (isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setDelta(period); 
+            setLoop(loop+1);
+        }
+    }
+
     return(
         <div id="Presentation" className={styles.presentation}>
             <h4><strong>Bem-vindo ao meu Portfólio</strong></h4>
-            <h1>Olá, eu sou o Yuri Tropeia!</h1>
+            <h1>Olá, eu sou {text}</h1>
             <p>
                 Apaixonado por tecnologia com formação em Engenharia de Gestão. <br/>
                 Com uma sólida base acadêmica e uma paixão inabalável por resolver problemas complexos, <br/>
